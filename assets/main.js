8488c8179336ba722a7b35b06fea8372f261fec5
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-04-12 19:28:30 +0200

    half milestone 2

--- assets/main.js
+++ assets/main.js
@@ -6,14 +6,17 @@ var app = new Vue ({
     selectedMovie: '',
     movies:[],
     selectedMovies:[],
-    currentIndex: 0
+    currentIndex: 0,
+    title: '',
+    language: '',
+    vote: '',
+    flagPath: ''
   },
   mounted() {
     axios.get('https://api.themoviedb.org/3/movie/popular?api_key=46a985f8a6499d16bf2fd0c1064f7714&language=en-US&page=1')
     .then((response) => {
       this.movies = response.data.results;
       this.selectedMovies = this.movies;
-
     })
   },
   methods: {
@@ -28,5 +31,12 @@ var app = new Vue ({
         }
       });
     },
+    setCurrentInfo: function (index) {
+      this.title = this.selectedMovies[index].title;
+      this.language = this.selectedMovies[index].original_language;
+      this.vote = this.selectedMovies[index].vote_average;
+      this.flagPath = `https://www.countryflags.io/${this.language}/flat/64.png`;
+
+    }
   }
 });
