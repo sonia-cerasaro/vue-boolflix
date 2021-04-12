a44f2c28e094ef29e20d5734a04d1256ccb04fc7
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-04-12 17:09:10 +0200

    1 milestone almost done

--- assets/main.js
+++ assets/main.js
@@ -3,16 +3,33 @@ Vue.config.devtools = true;
 var app = new Vue ({
   el: '#root',
   data: {
-    movies: []
-
+    selectedMovie: '',
+    movies:[],
+    selectedMovies:[],
+    currentIndex: 0
   },
   mounted() {
     axios.get('https://api.themoviedb.org/3/movie/popular?api_key=46a985f8a6499d16bf2fd0c1064f7714&language=en-US&page=1')
     .then((response) => {
-      this.movies.push(response.data.results);
+      this.movies = response.data.results;
+      this.selectedMovies = this.movies;
+
+      console.log(response.data.results);
     })
   },
-  searchMovie: () => {
+  methods: {
+
+    setSelectedMovies: function () {
+      this.selectedMovies = [];
+      this.movies.forEach((movie) => {
+        if (movie.title.toLowerCase().includes(this.selectedMovie.toLowerCase())) {
+          this.selectedMovies.push(movie);
+        } else if (this.selectedMovie == '') {
+          this.selectedMovies = this.movies;
+        }
+      });
+    },
+
 
   }
 });
