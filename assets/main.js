c659b3c328131719ef13de06f39f19d4cea7ec7d
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-04-13 14:54:24 +0200

    milestone 2 completed

--- assets/main.js
+++ assets/main.js
@@ -4,39 +4,51 @@ var app = new Vue ({
   el: '#root',
   data: {
     selectedMovie: '',
-    movies:[],
-    selectedMovies:[],
+    arrayMoviesAndTvShows: [],
+    selectedMovies: [],
     currentIndex: 0,
     title: '',
     language: '',
     vote: '',
-    flagPath: ''
+    flagPath: '',
+    uri: 'https://api.themoviedb.org/3',
+    api_key: '46a985f8a6499d16bf2fd0c1064f7714',
   },
   mounted() {
-    axios.get('https://api.themoviedb.org/3/movie/popular?api_key=46a985f8a6499d16bf2fd0c1064f7714&language=en-US&page=1')
+
+    axios.get(`${this.uri}/movie/popular?api_key=${this.api_key}&language=it-IT&page=1`)
     .then((response) => {
-      this.movies = response.data.results;
-      this.selectedMovies = this.movies;
-    })
+      this.arrayMoviesAndTvShows = response.data.results;
+    });
+    axios.get(`${this.uri}/tv/popular?api_key=${this.api_key}&language=it-IT&page=1`)
+    .then((response) => {
+      this.arrayMoviesAndTvShows = [...this.arrayMoviesAndTvShows, ...response.data.results];
+      this.selectedMovies = this.arrayMoviesAndTvShows;
+    });
   },
   methods: {
-
     setSelectedMovies: function () {
       this.selectedMovies = [];
-      this.movies.forEach((movie) => {
-        if (movie.title.toLowerCase().includes(this.selectedMovie.toLowerCase())) {
+      this.arrayMoviesAndTvShows.forEach((movie) => {
+        if (this.getTitle(movie).toLowerCase().includes(this.selectedMovie.toLowerCase())) {
           this.selectedMovies.push(movie);
         } else if (this.selectedMovie == '') {
-          this.selectedMovies = this.movies;
+          this.selectedMovies = this.arrayMoviesAndTvShows;
         }
       });
     },
     setCurrentInfo: function (index) {
-      this.title = this.selectedMovies[index].title;
+      this.title = this.getTitle(this.selectedMovies[index]);
       this.language = this.selectedMovies[index].original_language;
       this.vote = this.selectedMovies[index].vote_average;
-      this.flagPath = `https://www.countryflags.io/${this.language}/flat/64.png`;
-
-    }
+      this.flagPath = `https://www.unknown.nu/flags/images/${this.language}-100`;
+    },
+    getTitle: function (obj) {
+      if (obj.title) {
+        return obj.title;
+      }else{
+        return obj.name;
+      }
+    },
   }
 });
