b892ac800b66de5e6c57c57e96a49df871c3585f
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-04-13 15:41:54 +0200

    CSS some style

--- assets/main.js
+++ assets/main.js
@@ -13,14 +13,15 @@ var app = new Vue ({
     flagPath: '',
     uri: 'https://api.themoviedb.org/3',
     api_key: '46a985f8a6499d16bf2fd0c1064f7714',
+    lang: 'it-IT'
   },
   mounted() {
 
-    axios.get(`${this.uri}/movie/popular?api_key=${this.api_key}&language=it-IT&page=1`)
+    axios.get(`${this.uri}/movie/popular?api_key=${this.api_key}&language=${this.lang}&page=1`)
     .then((response) => {
       this.arrayMoviesAndTvShows = response.data.results;
     });
-    axios.get(`${this.uri}/tv/popular?api_key=${this.api_key}&language=it-IT&page=1`)
+    axios.get(`${this.uri}/tv/popular?api_key=${this.api_key}&language=${this.lang}&page=1`)
     .then((response) => {
       this.arrayMoviesAndTvShows = [...this.arrayMoviesAndTvShows, ...response.data.results];
       this.selectedMovies = this.arrayMoviesAndTvShows;
